9c53c62dee6401a10f598d3d7b6d834a3c899498
GDL75 <gsdl@free.fr>
2025-08-31 11:28:46 +0200

    ajout du test stats

---
+++ routes/tests/stats.test.js
@@ -0,0 +1,16 @@
+const request = require("supertest");
+const app = require("../../app");
+const mongoose = require("mongoose");
+
+// teste le nombre de posts du user "Az" (on attend : 2)
+it("GET /stats/PpnNwBt53PUced_mgP6R_B0u2RoQu2LV", async () => {
+  const res = await request(app).get("/stats/PpnNwBt53PUced_mgP6R_B0u2RoQu2LV");
+
+  expect(res.statusCode).toBe(200);
+  expect(res.body.stats.fromUser.nbPosts).toBe(2);
+});
+
+// pour éviter le message d'alerte en jaune à l'issue du test
+afterAll(async () => {
+  await mongoose.connection.close(); // ferme la connexion DB
+});
